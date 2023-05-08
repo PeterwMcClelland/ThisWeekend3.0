cbdc20dcb2d57a7ed46b6ed55f7c1fbded247ee3
Peter McClelland <peterwmcclelland@gmail.com>
2023-05-07 20:15:43 -0500

    add data to mongodb

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 5002;
 
 
-mongoose.connect( process.env.MONGODB_URL, {
+mongoose.connect( 'mongodb+srv://peterwmcclelland:K2gi77gbeYH5jrZR@cluster0.xzibbyr.mongodb.net/', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -16,14 +16,14 @@ app.use(express.json());
 
 app.use(cors()); 
 
-
+// Use the /api/signup endpoint
 const signupRouter = require('./src/components/SignUp/signup');
 app.use('/api/signup', signupRouter);
 
 const loginRouter = require('./src/components/LoginForm/login');
 app.use('/api/login', loginRouter);
 
-
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
