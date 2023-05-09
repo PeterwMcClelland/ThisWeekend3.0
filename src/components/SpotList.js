a6065a725b9d374f1fc9ec0862e5b32e50ea05da
Peter McClelland <peterwmcclelland@gmail.com>
2023-05-09 16:30:01 -0500

    background img css

--- src/components/SpotList.js
+++ src/components/SpotList.js
@@ -1,48 +1,3 @@
-// import React, { useEffect, useState } from 'react';
-// import axios from 'axios';
-
-// function SpotList() {
-//   const [spots, setSpots] = useState([]);
-
-//   useEffect(() => {
-//     axios.get('http://localhost:5002/api/spots')
-//       .then(res => {
-//         setSpots(res.data);
-//       })
-//       .catch(err => {
-//         console.error(err);
-//       });
-//   }, []);
-
-//   return (
-//     <div>
-//       <h1>Spot List</h1>
-//       <table>
-//         <thead>
-//           <tr>
-//             <th>ID</th>
-//             <th>Name</th>
-//             <th>Address</th>
-//           </tr>
-//         </thead>
-//         <tbody>
-            
-//           {spots.map(spot => (
-//             <tr key={spot.id}>
-//               <td>{spot.id}</td>
-//               <td>{spot.name}</td>
-//               <td>{spot.address}</td>
-//             </tr>
-//           ))}
-//         </tbody>
-//       </table>
-//     </div>
-//   );
-// }
-
-// export default SpotList;
-
-
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
@@ -76,7 +31,7 @@ function SpotList() {
         <thead>
           <tr>
             <th>ID</th>
-            <th>Image</th> {/* Add this line */}
+            <th>Image</th> 
             <th>Name</th>
             <th>Address</th>
             <th>Info</th>
@@ -97,6 +52,7 @@ function SpotList() {
         </tbody>
       </table>
     </div>
+    
   );
 }
 
