ac38dfda9adb217c0018946aab33c423b4fe3087
Peter McClelland <peterwmcclelland@gmail.com>
2023-05-14 16:27:58 -0500

    debugg

--- server.js
+++ server.js
@@ -16,7 +16,11 @@ mongoose.connect(process.env.MONGODB_URI, {
 app.use(express.json());
 app.use(cors());
 
+app.use(express.static(path.join(__dirname, 'client/build')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 
 app.get('/api/spots', async (req, res) => {
