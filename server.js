e522140bad7806f9321fafe46ed6cf2297568070
Peter McClelland <peterwmcclelland@gmail.com>
2023-05-18 19:42:11 -0500

    CORS update

--- server.js
+++ server.js
@@ -67,6 +67,68 @@
 
 
 
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const cors = require('cors');
+// const app = express();
+// const path = require('path');
+// const PORT = process.env.PORT || 5002;
+// require('dotenv').config();
+// const uri = process.env.MONGODB_URI;
+
+// mongoose.connect(process.env.MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+// app.use(express.json());
+// app.use(cors());
+
+// app.use(express.static(path.join(__dirname, 'client/build')));
+
+// app.get('/manifest.json', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'client/build', 'manifest.json'));
+// });
+
+// app.get('/api/spots', async (req, res) => {
+//   try {
+//     const spots = await mongoose.connection.db.collection('Spots').find({}).toArray();
+//     res.json(spots);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send('Internal server error');
+//   }
+// });
+
+// const signupRouter = require('./src/routes/SignUp/signup');
+// app.use('/api/signup', signupRouter);
+
+// const loginRouter = require('./src/routes/LoginForm/login');
+
+// const logRequestBody = (req, res, next) => {
+//   console.log('Request Body:', req.body);
+//   next();
+// };
+
+// app.use('/api/login', logRequestBody, loginRouter);
+
+// app.use(express.static(path.join(__dirname, '/client/build')));
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "client/build")));
+
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "client/build", "index.html"));
+//   });
+// }
+
+
+// app.listen(PORT, () => {
+//   console.log(`Server listening on port ${PORT}`);
+// });
+
+
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -81,8 +143,8 @@ mongoose.connect(process.env.MONGODB_URI, {
   useUnifiedTopology: true,
 });
 
+app.use(cors());  // Make sure you use the CORS middleware here, before your routes
 app.use(express.json());
-app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
@@ -121,7 +183,6 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
-
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
