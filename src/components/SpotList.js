b37bbf82516ee9c510232d7fc98e78d385651ed2
Peter McClelland <peterwmcclelland@gmail.com>
2023-05-25 17:08:55 -0500

    .env path

--- src/components/SpotList.js
+++ src/components/SpotList.js
@@ -7,6 +7,7 @@ import { faStar as regularStar } from '@fortawesome/free-regular-svg-icons';
 
 const path = require('path');
 
+
 require("dotenv").config({ path: path.resolve(__dirname, './../.env') });
 
 const copyPin = <FontAwesomeIcon icon={faCopy} />;
