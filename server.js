ddacc0ff4159bdea57f8d73c5d8d605e95c5c2f7
Peter McClelland <peterwmcclelland@gmail.com>
2023-07-05 22:01:19 -0500

    code cleaning

--- server.js
+++ server.js
@@ -1,60 +1,56 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
-const path = require('path');
+const path = require("path");
 const PORT = process.env.PORT;
-require('dotenv').config();
+require("dotenv").config();
 const uri = process.env.MONGODB_URI;
 
-const apiRoutes = require('./services/api');
+const apiRoutes = require("./services/api");
 
-app.use(cors()); 
-app.use('/api', apiRoutes);
-
-
-
-
+app.use(cors());
+app.use("/api", apiRoutes);
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-})
+});
 
 app.use(express.json());
 
-app.get('/api/spots', async (req, res) => {
+app.get("/api/spots", async (req, res) => {
   try {
-    const spots = await mongoose.connection.db.collection('Spots').find({}).toArray();
+    const spots = await mongoose.connection.db
+      .collection("Spots")
+      .find({})
+      .toArray();
     res.json(spots);
   } catch (error) {
     console.error(error);
-    res.status(500).send('Internal server error');
+    res.status(500).send("Internal server error");
   }
-  
 });
 
-const signupRouter = require('./src/routes/SignUp/signup');
-app.use('/api/signup', signupRouter);
+const signupRouter = require("./src/routes/SignUp/signup");
+app.use("/api/signup", signupRouter);
 
-const loginRouter = require('./src/routes/LoginForm/login');
+const loginRouter = require("./src/routes/LoginForm/login");
 
 const logRequestBody = (req, res, next) => {
-  console.log('Request Body:', req.body);
+  console.log("Request Body:", req.body);
   next();
 };
 
-app.use('/api/login', logRequestBody, loginRouter);
-
+app.use("/api/login", logRequestBody, loginRouter);
 
 if (process.env.NODE_ENV === "production") {
   // Serve any static files
   app.use(express.static(path.join(__dirname, "client/build")));
   // Handle React routing, return all requests to React app
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
-  
 }
 
 app.listen(PORT, () => {
