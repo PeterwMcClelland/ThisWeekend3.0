6b8283f81de063c14f72558fb0c9cdeb9879ee1f
Peter McClelland <peterwmcclelland@gmail.com>
2023-07-17 22:48:51 -0500

    code cleaning

--- src/routes/LoginForm/login.js
+++ src/routes/LoginForm/login.js
@@ -32,40 +32,41 @@
 
 // module.exports = router;
 
-
-const express = require('express');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
 
 // Load the User model
-const User = require('../../models/User');
+const User = require("../../models/User");
 
 // Login endpoint
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   const { username, password } = req.body;
 
   try {
     // Check if the username exists
     const user = await User.findOne({ username });
     if (!user) {
-      return res.status(400).json({ error: 'Invalid username' });
+      return res.status(400).json({ error: "Invalid username" });
     }
 
     // Compare the provided password with the hashed password in the database
     const passwordMatch = await bcrypt.compare(password, user.password);
     if (!passwordMatch) {
-      return res.status(400).json({ error: 'Invalid password' });
+      return res.status(400).json({ error: "Invalid password" });
     }
 
     // If username and password match, generate a JWT
-    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
 
     // Send success response along with the token
-    res.status(200).json({ token, message: 'Login successful' });
+    res.status(200).json({ token, message: "Login successful" });
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'Internal server error' });
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
